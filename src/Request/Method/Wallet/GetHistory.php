b4f647dda7b1a09a0a657765670d779a4e14a0e4
zorn-v <zorn7@yandex.ru>
2018-04-24 14:06:22 +1000

    Last electrum history compat
    3.1.3 history compatibility

--- src/Request/Method/Wallet/GetHistory.php
+++ src/Request/Method/Wallet/GetHistory.php
@@ -5,6 +5,7 @@ namespace Electrum\Request\Method\Wallet;
 use Electrum\Request\AbstractMethod;
 use Electrum\Request\MethodInterface;
 use Electrum\Response\Model\Wallet\Transaction;
+use Electrum\Response\Exception\BadResponseException;
 
 /**
  * Wallet history. Returns the transaction history of your wallet.
@@ -23,11 +24,19 @@ class GetHistory extends AbstractMethod implements MethodInterface
      *
      * @return HistoryResponse
      * @throws \Electrum\Request\Exception\BadRequestException
-     * @throws \Electrum\Response\Exception\ElectrumResponseException
+     * @throws \Electrum\Response\Exception\BadResponseException
      */
     public function execute(array $optional = [])
     {
         $data = $this->getClient()->execute($this->method, $optional);
+        if (!is_array($data)) {
+            $data = json_decode($data, true);
+            if (isset($data['transactions'])) {
+                $data = $data['transactions'];
+            } else {
+                throw new BadResponseException('Cannot get history');
+            }
+        }
         $transactions = [];
         foreach ($data as $transaction) {
             $transactions[] = $this->hydrate(new Transaction(), $transaction);
